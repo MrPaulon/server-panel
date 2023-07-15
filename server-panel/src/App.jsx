f1634b97295f05ad90bbf4dbbf15c549f3a4d786
Paul <76972320+MrPaulon@users.noreply.github.com>
2023-07-15 19:24:10 +0200

    🔨 The base of the window
    Adding the glass effect & adding the background image

--- server-panel/src/App.jsx
+++ server-panel/src/App.jsx
@@ -1,9 +1,14 @@
-import './App.css'
+import './App.scss'
+import bg from './assets/img/bg-1.jpeg'
 
 function App() {
   return (
     <>
       <div className="App">
+        <img className='background' src={bg}/>
+        <div className='content'>
+          <div className='window'></div>
+        </div>
       </div>
     </>
   )
